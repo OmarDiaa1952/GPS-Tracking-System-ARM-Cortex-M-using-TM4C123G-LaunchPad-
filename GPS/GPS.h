bceaa4b8f87a8589f5413f25e7efa8287e8d7445
Ahmed Magdy <69248765+AMF777@users.noreply.github.com>
2021-06-04 01:35:42 +0200

    Add files via upload

---
+++ GPS/GPS.h
@@ -0,0 +1,5 @@
+void readGPS(char * lat, char * log);
+
+double calcDistance(volatile double latitude_1, volatile double logitude_1, volatile double latitude_2, volatile double logitude_2);
+
+double floatVal(char *c);
