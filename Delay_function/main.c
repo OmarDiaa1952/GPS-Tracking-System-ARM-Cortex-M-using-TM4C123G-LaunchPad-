4eca523e4a4d3198ec938563a52b702296b373cd
Eugeo101 <81474504+Eugeo101@users.noreply.github.com>
2021-06-04 01:11:29 +0200

    ADD Delay Functions
    modify Delay Functions to make it cope with good secounds as real life by for loobs not recursive

--- Delay_function/main.c
+++ Delay_function/main.c
@@ -1,19 +1,4 @@
-// Port F
-#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
-#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
-#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
-#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
-#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
-#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
-#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
-#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
-#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
-#define GPIO_LOCK_KEY           0x4C4F434B  // Unlocks the GPIO_CR register
-
-
-// Global Registers and Macro Functions for all Ports
-#define SYSCTL_RCGC2_R				  (*((volatile unsigned long *)0x400FE608))
-#define SYSCTL_PRGPIO_R			  	(*((volatile unsigned long *)0x400FEA08)) 	
+#include "tm4c123gh6pm.h" // that has all timer files
 
 // PortF intialization
 void init_PortF(void){ 
@@ -32,7 +17,6 @@ void init_PortF(void){
   GPIO_PORTF_DEN_R = 0x1F;          		  // 10) enable digital I/O on PF4-0
 }
 
-#include "tm4c123gh6pm.h" // that has all timer files
 void init_Timer_delay(void)
 {
 	NVIC_ST_CTRL_R = NVIC_ST_CTRL_R & ~(0x00000001); //disable Timer
@@ -41,47 +25,50 @@ void init_Timer_delay(void)
 	NVIC_ST_CTRL_R |= 0x5; //to put at source clk 1 to get PROCESOR CLK NOT its 8th only and enable Timer
 }
 void delay(int s, int d);
+void delay_fraction(int d);
 
-unsigned int frac = 0;
 unsigned int counter = 0;
 
 int main()
 {
 init_PortF();
 
+GPIO_PORTF_DATA_R |= 0x02;   //Read On
 
 	while (1)
 	{
+		delay(2, 0);
 		counter++;
-		GPIO_PORTF_DATA_R &= ~(0x0E); //clear leds
-		GPIO_PORTF_DATA_R |= 0x02;   //Read On
-		delay(2, 3);
 		GPIO_PORTF_DATA_R ^= 0x02;
 	}
 }
 
 void delay(int s, int d)
 {
-	init_Timer_delay();
-	if (s == 1) //base case of recursive
-	{
-		if ((NVIC_ST_RELOAD_R & 0x10000) == 1)
-			;
-		//fraction part
-		NVIC_ST_CTRL_R = NVIC_ST_CTRL_R & ~(0x00000001);
-		NVIC_ST_RELOAD_R = (d / 10) * (16000000 - 1);
-		NVIC_ST_CURRENT_R = 0; 
-		NVIC_ST_CTRL_R |= 0x5;
-		if ((NVIC_ST_RELOAD_R & 0x10000) == 1)
+		init_Timer_delay();
+		if (s == 1) //base case of recursive
 		{
+			while ((NVIC_ST_CTRL_R & 0x00010000) == 0) {}
+			if (d == 0)
+			{
+				return;
+			}
+			delay_fraction(d);
 			return;
 		}
-	}
-	else //recruisive body
-	{
-		if ((NVIC_ST_RELOAD_R & 0x10000) == 1)
+		else //recruisive body
 		{
+			while ((NVIC_ST_CTRL_R & 0x00010000) == 0) {}
 			delay(s-1, d);
 		}
-	}
+}
+void delay_fraction(int d)
+{
+	//fraction part
+			NVIC_ST_CTRL_R = NVIC_ST_CTRL_R & ~(0x00000001);
+			NVIC_ST_RELOAD_R = 0.5 * (16000000 - 1);
+			NVIC_ST_CURRENT_R = 0; 
+			NVIC_ST_CTRL_R |= 0x5;
+			while ((NVIC_ST_CTRL_R & 0x00010000) == 0) {}
+			return;
 }
