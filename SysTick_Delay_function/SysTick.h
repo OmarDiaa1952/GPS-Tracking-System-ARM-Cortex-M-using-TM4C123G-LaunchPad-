e7cbb7a94877b3b336cf64ad969c447889734e6d
AhmedAyman101 <81474504+AhmedAyman101@users.noreply.github.com>
2021-06-04 17:08:54 +0200

    Update SysTick.h

--- SysTick_Delay_function/SysTick.h
+++ SysTick_Delay_function/SysTick.h
@@ -1,12 +1,11 @@
 #include "tm4c123gh6pm.h" // that has all timer files
 
-// PortF intialization
-void init_PortF(void);
-
 // SysTick intialization
 void init_Timer_delay(void);
 
 //delay Functions using SysTick
-void delay(int s, int d);
 void Delay_Of_Fractions(int frac);
+void delay(int s, int d);
+
+//delay MicroSeconds
 void hundredMicroSecounds(void);
