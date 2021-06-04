29497b03318f36e518c88a129032f479355f45ac
Eugeo101 <81474504+Eugeo101@users.noreply.github.com>
2021-06-04 15:10:00 +0200

    Update SysTick.c
    Solved the Fraction Problem using loops of 0.1 * 1 second of system counter

--- SysTick_Delay_function/SysTick.c
+++ SysTick_Delay_function/SysTick.c
@@ -16,8 +16,8 @@ void init_PortF(void){
   GPIO_PORTF_DEN_R = 0x1F;          		  // 10) enable digital I/O on PF4-0
 }
 
-unsigned int frac = 0;     // to calculate fraction of seconds
 unsigned int counter = 0;  //as 2.3 secound pass counter increase and led toogle
+unsigned int i = 0; 		   //for the for loop of hundred micro seconds
 
 void init_Timer_delay(void) //intialization of SysTick Counter Timer
 {
@@ -27,36 +27,34 @@ void init_Timer_delay(void) //intialization of SysTick Counter Timer
 	NVIC_ST_CTRL_R |= 0x5; 													 //to put at source clk 1 to get PROCESOR CLK NOT its 8th only and enable Timer
 }
 
-void delay(int s, int d) //recursive function of the delay
+void hundredMicroSecounds(void)
 {
-		init_Timer_delay();
-		if (s == 1) //base case of recursive
-		{
-			while ((NVIC_ST_CTRL_R & 0x00010000) == 0) {}
-			if (d == 0)
-			{
-				return;
-			}
-			delay_fraction(d);
-			return;
-		}
-		else //recruisive body
-		{
-			while ((NVIC_ST_CTRL_R & 0x00010000) == 0) {}
-			delay(s-1, d);
-		}
-}
-void delay_fraction(int d)
-{
-	//fraction part
-			NVIC_ST_CTRL_R = NVIC_ST_CTRL_R & ~(0x00000001);
-			NVIC_ST_RELOAD_R = 0.5 * (16000000 - 1);
-			NVIC_ST_CURRENT_R = 0; 
-			NVIC_ST_CTRL_R |= 0x5;
-			while ((NVIC_ST_CTRL_R & 0x00010000) == 0) {}
-			return;
+	NVIC_ST_CTRL_R = NVIC_ST_CTRL_R & ~(0x00000001); //disable Timer
+	NVIC_ST_RELOAD_R = (1600000 - 1); //to make 1 secound as our Tiva has clk = 16 MHZ
+	NVIC_ST_CURRENT_R = 0; //to clear counter value and underflow flag of counter
+	NVIC_ST_CTRL_R |= 0x5; //to put at source clk 1 to get PROCESOR CLK NOT its 8th only and enable Timer
 }
 
+void delay(int s, int d) //recursive function of the delay
+{
+	for (i = 0; i < s; i++)
+	{
+		init_Timer_delay();
+		while ((NVIC_ST_CTRL_R & 0x00010000) == 0) {}
+	}
+	delay_fraction(d);
+	return;
+}
+
+void delay_fraction(int frac)
+{
+	for (i = 0; i < frac; i++)
+	{
+		hundredMicroSecounds();
+		while ((NVIC_ST_CTRL_R & 0x00010000) == 0) {}
+	}
+	return;
+}
 
 int main()
 {
@@ -66,7 +64,7 @@ GPIO_PORTF_DATA_R |= 0x02;   //Read On
 
 	while (1)
 	{
-		delay(2, 0); //example to test delay function 2.0 secound then counter increase and led on
+		delay(2, 5); //example to test delay function 2.5 secound then counter increase and led on
 		counter++;
 		GPIO_PORTF_DATA_R ^= 0x02;
 	}
