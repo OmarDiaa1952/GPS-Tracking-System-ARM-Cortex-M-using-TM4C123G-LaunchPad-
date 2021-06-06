c9535a36a5144bdc2165fab090d784fcea7092ac
ahmed-elgerby <84804725+ahmed-elgerby@users.noreply.github.com>
2021-06-06 17:15:42 +0200

    Update UART.h

--- UART/UART.h
+++ UART/UART.h
@@ -19,3 +19,25 @@ unsigned char UART1_ReadChar(void);
 void UART1_WriteString(char *str);
 
 void UART1_ReadString(char *str, char stopCh);
+
+//UART5
+void UART5_Init(unsigned long baudRate);
+
+void UART5_WriteChar(unsigned char ch);
+
+unsigned char UART5_ReadChar(void);
+
+void UART5_WriteString(char *str);
+
+void UART5_ReadString(char *str, char stopCh);
+
+//UART7
+void UART7_Init(unsigned long baudRate);
+
+void UART7_WriteChar(unsigned char ch);
+
+unsigned char UART7_ReadChar(void);
+
+void UART7_WriteString(char *str);
+
+void UART7_ReadString(char *str, char stopCh);
