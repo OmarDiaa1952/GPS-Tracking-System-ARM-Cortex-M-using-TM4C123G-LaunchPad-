178b91c1170065fd7990280da7fe64414e5f9898
Ahmed Magdy <69248765+AMF777@users.noreply.github.com>
2021-06-06 15:30:11 +0200

    Update GPS.h

--- GPS/GPS.h
+++ GPS/GPS.h
@@ -1,5 +1,12 @@
-void readGPS(char * lat, char * log);
+int readGPS(char * lat, char * log);
 
 double calcDistance(volatile double latitude_1, volatile double logitude_1, volatile double latitude_2, volatile double logitude_2);
 
 double floatVal(char *c);
+
+void readGPSDouble(volatile double * dlat,volatile double * dlog);
+
+void concat(char * holder, char * lat, char *log);
+
+void splitAndStoreDouble(char * data, volatile double * startLat, volatile double * startLog, volatile double * endLat, volatile double * endLog);
+	
