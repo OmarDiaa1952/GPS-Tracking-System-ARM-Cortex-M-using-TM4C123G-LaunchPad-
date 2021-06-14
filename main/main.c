9f4a929c8a0dea82c7be39563ca8b01437c92e1c
Ahmed Magdi <ammhaa2012@gmail.com>
2021-06-14 11:14:51 +0200

    Tiva is now connected to UART0
    -Due to the wifi module problem we replaced it with laptop with python code and tiva is now connected to UART0
    
    -Modified the readings from ddmm.mm to dd.mmmm

--- main/main.c
+++ main/main.c
@@ -6,26 +6,28 @@
 #include "DISPLAY.h"
 #include "LCD.h"
 #include "SERVER.h"
+#include "math.h"
 
 char latitude[15];
 char longitude[15];
 char lat_log[30];
+char lat_log_dis[50];
+int i = 0;
 //-------------------------------
 volatile double fractions = 0; 
 //-------------------------------
-
+volatile double totalDistance = 0.0; //Total distance
 char data[100]; //General data holder
 
 void LCD_message_R1(char *data);
 void LCD_message_Dis(double dis);
-void LCD_LatLogDis(char *lat,char *log,volatile double * dis);
+void LCD_LatLogDis(double lat,double log,volatile double dis);
 void LCD_Log(char *data);
 void led(int color);
 int main(){
 	char lat[15];
 	char log[15];
-	volatile double totalDistance = 0.0; //Total distance
-	volatile double startEndCoords[4] = {0.0,0.0,0.0,0.0}; //Hold start and end point (latitudeStart,logStart,latEnd,logEnd)
+	
 	volatile int mode = 0;// 0-> Inactive, 1-> Initialization, 2-> Positioning, 3-> Active, 4-> Pre-reset
 	volatile double distance = 0.0; //Hold distance every reading
 	volatile double coords[4]= {0.0,0.0,0.0,0.0}; //Hold every 2 points (2 lats + 2 longs)
@@ -34,13 +36,13 @@ int main(){
 	
 	UART5_Init(9600);
 	UART7_Init(9600); //Change to 115200
+	UART0_Init(9600);
 	initPortF();
 	initPortD();
 	LCD_INIT();
 	LCD_CMD(Function_set_8bit);
 	
 	LCD_message_R1("Inactive");
-	//countingDistance(20.3,&fractions);
 	delay(1,0);
 	while(1){
 		switch(mode){
@@ -54,10 +56,11 @@ int main(){
 					while(readGPS(lat,log)){}
 					concat(lat_log,lat,log);
 					led(12); //Turquoise
+					command('I',lat_log);
 					done = 1;
 				}else{
-					UART7_ReadString(data,'\n'); 
-					while(data[0] != '$'){UART7_ReadString(data,'\n');}
+					UART0_ReadString(data,'\n'); 
+					while(data[0] != '$'){UART0_ReadString(data,'\n');}
 					if(data[1] == 'F'){done = 0;break;}
 					if(data[1] == 'S'){
 						mode = 2;
@@ -69,18 +72,17 @@ int main(){
 				
 				case 2: //Positioning mode
 					while(readGPS(lat,log)){}
-					concat(lat_log,lat,log);
 					led(10);
-					if( (floatVal(lat) == startEnd[0]) && (floatVal(log) == startEnd[1]) ){
-						coords[0] = floatVal(lat);
-						coords[1] = floatVal(log);
+						coords[0] = convertCoords(floatVal(lat));
+						coords[1] = convertCoords(floatVal(log));
+					if( (fabs(coords[0] - startEnd[0]) < 0.0002) && (fabs(coords[1] - startEnd[1]) < 0.0002) ){
 						command('S',lat_log);
 						delay(3,5);
 						LCD_message_R1("Active");
 						done = 1;
 						mode = 3;
-					}else{
-						delay(2,0);
+					}else{		
+						delay(1,0);
 						mode = 2;
 					}
 			
@@ -88,30 +90,62 @@ int main(){
 					
 			case 3: //Active mode
 				while(readGPS(lat, log)){}
-				concat(lat_log,lat,log);
+				concat(lat_log,lat,log);				
 				led(8);
-				if(done){
-					coords[2] = floatVal(lat);
-					coords[3] = floatVal(log);
-					done = 0;
-				}else{
-					coords[0] = floatVal(lat);
-					coords[1] = floatVal(log);
-					done = 1;
-				}
+					
+					if(coords[2] == 0.0){
+						coords[2] = convertCoords(floatVal(lat));
+						coords[3] = convertCoords(floatVal(log));
+					}else{
+						coords[0] = coords[2];
+						coords[1] = coords[3];
+						coords[2] = convertCoords(floatVal(lat));
+						coords[3] = convertCoords(floatVal(log));
+					}
 				
-				distance = calcDistance(coords[0],coords[1],coords[2],coords[3]);	
-				totalDistance += distance;
-				countingDistance(distance,&fractions);
-				if( (floatVal(lat) == startEnd[2]) && (floatVal(log) == startEnd[3]) ){
-					LCD_LatLogDis(lat,log,&distance);
+				UART0_WriteString("Distance between :");
+					UART0_WriteString(toString(coords[0],7));
+				UART0_WriteString(", ");
+				UART0_WriteString(toString(coords[1],7));
+				UART0_WriteString(" AND ");
+				UART0_WriteString(toString(coords[2],7));
+				UART0_WriteString(", ");
+				UART0_WriteString(toString(coords[3],7));
+					
+				
+				distance = calcDistance(coords[0],coords[1],coords[2],coords[3]);
+				distance = distance < 1.0?0.0:distance;
+				totalDistance = totalDistance + distance;
+					
+				UART0_WriteString(" is ");
+					UART0_WriteString(toString(distance,4));
+				UART0_WriteString(" AND total distance = ");
+				UART0_WriteString(toString(totalDistance,4));
+					UART0_WriteString("\n");
+				
+				
+				
+				
+				//countingDistance(distance,&fractions);
+				
+				
+				LCD_LatLogDis(coords[2],coords[3],totalDistance);				
+				if( (fabs(coords[2] - startEnd[2]) < 0.0002) && (fabs(coords[3] - startEnd[3]) < 0.0002) ){
 					delay(2,1);
-					command('E',lat_log);
+					UART0_WriteString("@E,");
+					UART0_WriteString(lat_log);
+					UART0_WriteString(",");
+					UART0_WriteString(toString(totalDistance,3));
+					UART0_WriteString("\n");
+				
 					LCD_message_Dis(totalDistance);
 					mode = 4;
 				}else{
-					command('C',lat_log);
-					LCD_LatLogDis(lat,log,&distance);
+					UART0_WriteString("@C,");
+					UART0_WriteString(lat_log);
+					UART0_WriteString(",");
+					UART0_WriteString(toString(totalDistance,3));
+					UART0_WriteString("\n");					
 					mode = 3;
 				}
 				
@@ -152,10 +186,10 @@ void LCD_message_Dis(double dis){
 			delay_milli(15);
 			LCD_CMD(cursorOn);
 			delay_milli(15);
-			LCD_String(toStringAyman(dis));
+			LCD_String(toString(dis,2));
 			LCD_String(" m");
 }
-void LCD_LatLogDis(char *lat,char *log,volatile double * dis){
+void LCD_LatLogDis(double lat,double log,volatile double dis){
 			LCD_CMD(clear_display);
 			delay_milli(15);
 			LCD_CMD(FirstRow);
@@ -163,21 +197,21 @@ void LCD_LatLogDis(char *lat,char *log,volatile double * dis){
 			LCD_CMD(cursorOn);
 			delay_milli(15);
 			LCD_String("Lat = ");
-			LCD_String(lat);
+			LCD_String(toString(lat,5));
 			delay_milli(15);
 			LCD_CMD(SecondRow);
 			delay_milli(15);
 			LCD_CMD(cursorOn);
 			delay_milli(15);
 			LCD_String("Log = ");
-			LCD_String(log);
+			LCD_String(toString(log,5));
 			delay(2,0);
 			LCD_CMD(clear_display);
 			delay_milli(10);
 			LCD_CMD(FirstRow);
 			delay_milli(10);
 			LCD_String("Dis = ");
-			LCD_String(toStringAyman(*dis));
+			LCD_String(toString(dis,2));
 			LCD_String(" m");
 			delay(2,0);
 }
