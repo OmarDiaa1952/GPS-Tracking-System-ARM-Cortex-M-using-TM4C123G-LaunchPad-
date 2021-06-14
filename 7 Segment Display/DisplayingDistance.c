d7f5a88b71f150d981c6fc5af692b1ffbed8a967
AhmedAyman101 <81474504+AhmedAyman101@users.noreply.github.com>
2021-06-14 13:10:58 +0200

    Update DisplayingDistance.c
    add features

--- 7 Segment Display/DisplayingDistance.c
+++ 7 Segment Display/DisplayingDistance.c
@@ -1,7 +1,9 @@
-#include "DisplayingDistance.h"
+#include "DISPLAY.h"
+#include "TM4C123GH6PM.h"
 
 void pulse(void)  //make pulse for decoder to start counting
 {
+	/*
 	//put PD0 pin as 0 then delay 0.1 sec then 1 and delay 0.1 sec and return 0 for 0.1sec this is the pulse function and will genereate pulse
 	GPIO_PORTD_DATA_R &= ~(0x00000001);
 	delay(0, 1);  //delay 0.1 secound
@@ -9,6 +11,13 @@ void pulse(void)  //make pulse for decoder to start counting
 	delay(0, 1);  //delay 0.1 secound
 	GPIO_PORTD_DATA_R &= ~(0x00000001);
 	delay(0, 1);  //delay 0.1 secound >> so now drwawed the pulse of PD0 which will start the counter to increment in 0.3secounds
+	*/
+	
+	CLEAR(GPIO_PORTA_DATA_R,2);
+	delay(0, 1);
+	SET(GPIO_PORTA_DATA_R,2);
+	delay(0, 1);
+	CLEAR(GPIO_PORTA_DATA_R,2);
 	return;
 }
 
@@ -18,27 +27,29 @@ void resetCounters(void)   //reset all counters of the 3 decoders so seven segme
 	return;
 }
 
-void countingDistance(double L)    // L is distance between 2 points so it could be 2.23 so it's intger and fraction part and displayed on 7 segment
+
+/*void updateDisplay(double * td, int *dd) // td = 5.99 >> dd = 2 >>>>> 7segment 3aleha 5 >> 8
+{
+	while ((int)*td > *dd)
+	{
+		pulse();
+		*dd += *dd;
+	}
+}*/
+
+void countingDistance(double L,volatile double *fraction)    // L is distance between 2 points so it could be 2.23 so it's intger and fraction part and displayed on 7 segment
 {
 	int d = (int) L;									//intger part will be saved in d then displayed in line 97>>101
-	fractions = fractions +  (L - (int)L);  //fraction will be summed till it reach 1 then make pulse where its ranges 0.02>>>>1.98
+	*fraction = *fraction +  (L - (int)L);  //fraction will be summed till it reach 1 then make pulse where its ranges 0.02>>>>1.98
 	while(d)
 	{		
 		pulse(); //generate pulse the increase 7 segment number
 		d--;
 	}
-	if (fractions >=1)      //range from 0.02 to 1.98 is still need to be summed once more so won't go here in the loop but from 1>>>1.98 will be as 1 pulse
+	if (*fraction >=1)      //range from 0.02 to 1.98 is still need to be summed once more so won't go here in the loop but from 1>>>1.98 will be as 1 pulse
 	{
 		pulse();
-		fractions--;  //from 1>>1.98 minused by 1 so 0>>0.98 and be added again
+		*fraction -= 1;  //from 1>>1.98 minused by 1 so 0>>0.98 and be added again
 	}
 	return;
 }
-/*void countingDistance2(double * td, int *dd) // td = 5.99 >> dd = 2 >>>>> 7segment 3aleha 5 >> 8
-{
-	while ((int)*td > *dd)
-	{
-		pulse();
-		*dd = *dd + 1;
-	}
-}*/
