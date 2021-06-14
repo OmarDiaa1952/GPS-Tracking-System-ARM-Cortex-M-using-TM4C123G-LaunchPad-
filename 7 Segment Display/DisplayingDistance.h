3202eb811648a4cf9205e98fb071730635fe0db1
AhmedAyman101 <81474504+AhmedAyman101@users.noreply.github.com>
2021-06-14 12:58:20 +0200

    Update DisplayingDistance.h
    updateing code

--- 7 Segment Display/DisplayingDistance.h
+++ 7 Segment Display/DisplayingDistance.h
@@ -1,22 +1,8 @@
 #include "SYSTICK.h"
 #include "TM4C123GH6PM.h"
 
-// PortD intialization
-void init_PortD(void){ 
-  SYSCTL_RCGC2_R |= 0x00000008;           // 1) activate clock for Port D
-  while ((SYSCTL_PRGPIO_R & 0x08) == 0);	// 2) allow time for clock to start
-	
-  GPIO_PORTD_CR_R = 0xF;                 // 4) allow changes to PD3-0
-  GPIO_PORTD_AMSEL_R = 0x00;              // 5) disable analog on PD
-  GPIO_PORTD_PCTL_R = 0x00000000;         // 6) PCTL GPIO on PD7-0 disabled i won't use Aleternate function
-  GPIO_PORTD_DIR_R = 0x0F;                // 7) PD3-0 as output
-  GPIO_PORTD_AFSEL_R = 0x00;              // 8) disable alt funct on PD7-0
-  GPIO_PORTD_PUR_R = 0x00;                // 9) disable pull-up on PD3-0
-  GPIO_PORTD_DEN_R = 0x0F;          		  // 10) enable digital I/O on PD3-0
-}
-
 //functions for counting
 void pulse(void);
-void countingDistance(double L);
+void countingDistance(double L,volatile double *fraction);
 void resetCounters(void);
-double fractions = 0;
+
